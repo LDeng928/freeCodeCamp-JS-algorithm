1ac08b7ca82ba03c8fea5e293218e1dde1ea42e0
Ldeng928 <lideng928@gmail.com>
2021-12-29 13:06:46 -0500

    US phone number validator solution

---
+++ telephoneNumberValidator.js
@@ -0,0 +1,24 @@
+/* Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+ */
+
+function telephoneCheck(str) {
+  const usPhoneNumberPattern = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+  if (str.match(usPhoneNumberPattern)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(telephoneCheck('1 555)555-5555'));
